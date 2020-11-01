37100e582c834e14f3034f857fe6087a42600bbd
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-11-01 20:50:17 +0530

    1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,38 +1,38 @@
 import React, { useState } from 'react'
 const Stats = (props) => {
-    return (
+  if(props.total > 0)
+      return (
         <div>
         {props.text} {props.value}
         </div>
     );
+    
 }
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-    const [avg,setAvg]=useState(0)
-    const [all,setAll]=useState(0)
+        const all=good+bad+neutral;
+    const avg=(good-bad)/(good+bad+neutral);
+    const positive=(good * 100 /all)+"%";
   const handleGood = () =>{
-     const val=good + 1;
+     const val=good+1;
       setGood(val);
-      calOther();
+      // setAll(all+1);
+     
   }
   const handleNeutral = () =>{
     const val=neutral+1;
     setNeutral(val);
- calOther();
-}
+    // setAll(all+1);
+   
+  }
 const handleBad = () =>{
     const val=bad+1;
-    setBad(val);
-    calOther();    
-}
-const calOther = () => {
-    const newall=good+bad+neutral;
-    setAll(newall);
-    const newAvg = (good - bad) /all;
-    setAvg(newAvg);
+    setBad(val); 
+    // setAll(all+1);
+   
 }
 
 
@@ -41,10 +41,11 @@ const calOther = () => {
        <h1>Give Feedback</h1>
        <button type="button" onClick={handleGood}>Good</button> <button type="button" onClick={handleNeutral} >Neutral</button> <button type="button" onClick={handleBad} >Bad</button>
       <h2>Statistics</h2>
-     <Stats value={good} text="good"/>
-     <Stats value={neutral} text="neutral" />
-    <Stats value={bad} text="bad" />
-    <Stats value={avg} text="Average" />
+     <Stats value={good} total={all} text="good"/>
+     <Stats value={neutral} total={all} text="neutral" />
+    <Stats value={bad} total={all} text="bad" /> <Stats value={all} text="All" />
+    <Stats value={avg} total={all} text="Average" />
+    <Stats value={positive} total={all} text="Positive" />
       </div>
     )
   }
