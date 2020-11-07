16118c71558f2fcd6685dd69b906aa13f168f23f
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-11-07 19:31:31 +0530

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,31 @@
 import React, { useState } from 'react'
-
+const Persons = (props) => {
+  return (
+      <ul>
+          {props.persons.filter(person => person.name.toUpperCase().includes(props.newSearch.toUpperCase())).map(person => (
+          <Person key={person.id} name={person.name} number={person.number}  /> ))}
+      </ul> 
+  ) 
+}
+const Person = (props) => {
+  return (
+    <li>{props.name}  {props.number} </li>
+  )
+}
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',id:1}
+    { name: 'Arto Hellas', number: '040-123456',id:1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523',id:2 },
+    { name: 'Dan Abramov', number: '12-43-234345',id:3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122',id:4 }
   ])
+  const [newSearch , setnewSearch]=useState('')
+  const [personsToShow,setpersonsToShow] = useState([])
   const [ newName, setNewName ] = useState('')
-  const setName = (event) => {
-    
-    setNewName(event.target.value)
-    
-  }
+  const [ number, setNumber ] = useState('')
+ 
+  const setName = (event) =>setNewName(event.target.value);
+  const setnewNumber = (event) => setNumber(event.target.value);
   const checkduplicates = (person) => person.name === newName;
   const addName = (event) => {
     event.preventDefault();
@@ -20,33 +36,39 @@ const App = () => {
     else{
       const personObject ={
         name:newName,
+        number:number,
       id: Math.floor(Math.random() * 101)
       }
       setPersons([...persons, personObject])
-      console.log(persons);
-      
-    }
+      setNewName('');
+        }
     
   }
-  const Persons = () => {
-    return(
-    <div>{persons.map(person => <p id={person.id}>{person.name}</p>)} </div>
-    )
+  const handleSearchChange = (event)  =>{
+    setnewSearch(event.target.value);
   }
+  
+ 
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form><div>
+        fliter shown with  <input
+            value={newSearch}
+            onChange={handleSearchChange} />
+            <br />   </div>
         <div>
           name: <input value={newName} onChange={setName} />
         </div>
+        <div>number: <input value={number} type="number"  onChange={setnewNumber} /></div>
         <div>
           <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <Persons />
-  
+      <Persons persons={persons}
+        newSearch={newSearch} />
     </div>
   )
 }
