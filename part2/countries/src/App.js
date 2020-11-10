f76e39c1a1f27326be7143f792ea6026ce02da5e
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-11-10 15:23:42 +0530

    added 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,21 +1,93 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-const Countries = ({countries,search,showC,setShowC}) =>{
+
+const Countries = ({countries,search}) =>{
+  const [showCountry,setShowCountry]=useState();
+  const showDetail = (event) =>{
+    console.log(event.target.value);
+    const cont = countries.filter(country =>
+      country.name.includes(event.target.value)
+    );
+    console.log("cont: ", cont);
+    setShowCountry(cont[0]);
+  }
 if(countries.filter(country => country.name.toUpperCase().includes(search.toUpperCase())).length >=10)
 return (
   <p>
     please give more specification
   </p>
 );
+  else if(showCountry!== undefined){
+    console.log(showCountry);
+    return(
+      <Detail country={showCountry} />
+    )
+  }
   return(
-  <div>{  countries.filter(country => country.name.toUpperCase().includes(search.toUpperCase())).splice(0,10).map(c => (<p key={c.numericCode}>{c.name}</p>))}</div> 
+  <div>{  countries.filter(country => country.name.toUpperCase().includes(search.toUpperCase()))
+    .splice(0,10).map(c => (
+      (<DisplayCountry c={c} showDetail={showDetail} />) 
+      ))}</div> 
  
   ) ;
   }
+  const DisplayCountry= ({c,showDetail}) =>{
+    console.log(c);
+   return(
+    <div key={c.numericCode}>
+<p>{c.name}</p><button value={c.name} onClick={showDetail}>show</button>
+    </div>
+  )
+} 
+  
+const Detail = ({country}) => {
+  const [weather,setWeather]=useState()
+  
+  const weatherhook =() => {
+    console.log('effect')
+    axios
+      .get(process.env.REACT_APP_YOUR_ACESS_URL +country.name)
+      .then(response => {
+         setWeather(response.data)})
+      console.log("weather");
+      console.log(weather); 
+  }
+ useEffect(weatherhook,[])
+  return(
+<div>
+  <h1>{country.name}</h1>
+  <p>capital {country.capital}</p>
+  <p>population {country.population}</p>
+  <h2>Spoken Languages</h2>
+  <ul>
+  {country.languages.map(lang => <li>{lang.name}</li>)}
+  </ul>
+<img src = {country.flag} alt="loading ..." style={{width:"300px",height:"200px"}} />
+  <h2>Weather in {country.name}</h2>
+  <Weather weather={weather} />
+
+</div>
+  )
+}
+const Weather = ({weather}) => {
+  if(weather!==undefined)
+return (
+  <div><p><b>temperature : </b>{weather.current.temperature} Celcius
+  <img src={weather.current.weather_icons} style={{marginBottom:"30px"}} alt="loading..." />
+  <br />
+ <b>wind : </b> {weather.current.wind_speed} mph {weather.current.wind_diretcion}
+  </p> </div>
+)
+else 
+return (
+  <div>Weather undefined</div>
+)
+}
+
 const App = () =>{
     const [countries,setCountries]=useState([])
   const [search,setSearch]=useState('')
-  const[showC,setShowC]=useState([])
+ 
     const hook =() => {
         console.log('effect')
         axios
@@ -31,13 +103,14 @@ const App = () =>{
 const setNewSearch = (event) => {
   setSearch(event.target.value);
 }
+
 return(
   <div>
     <form>
       find countries <input value={search} onChange={setNewSearch} />
     </form>
-   {console.log('render', countries.length, 'countries') }
-   <Countries countries={countries} search={search} showC={showC} setShowC={setShowC} />
+  
+   <Countries countries={countries} search={search} />
   </div>
 
 );
