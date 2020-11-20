ea9da0d91c42cb9354b5eef0fbec07177ad745e8
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-11-20 16:59:18 +0530

    added 3.1 to 3.6

---
+++ part3/index.js
@@ -0,0 +1,81 @@
+const { response } = require('express')
+const express=require('express')
+const app = express()
+app.use(express.json())
+let persons=[
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": 1
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": 3
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
+ 
+
+  
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+  app.get('/api/info',(request,response) => {
+    const dateObject=new Date()
+      const info="Phonebook has 4 people\\n" + dateObject
+      response.json(info)
+  })
+  app.get('/api/persons/:id',(req,res) => {
+      uid=Number(req.params.id);
+     const person= persons.find(person => person.id === uid)
+     res.json(person)
+  })
+  app.delete('api/persons/:id',(req,res) => {
+    uid=Number(req.params.id);
+   const person= persons.filter(person => person.id === uid)
+   res.status(204).end()
+})
+app.post('/api/persons', (request, response) => {
+  const body=request.body
+  const name=body.name
+  if(!body.name){
+    return response.status(404).json({
+      error:'Please fill name'
+    })
+  }
+  if(persons.filter(person => person.name === name))
+  { 
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+
+  }
+   const id = persons.length > 0
+    ? Math.max(...persons.map(n => n.id)) 
+    : 0
+    const perObject={
+      name:body.name,
+      number:body.number,
+      id:id+1
+    }
+    console.log(perObject)
+    persons=persons.concat(perObject)
+   response.json(persons)
+  })
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
