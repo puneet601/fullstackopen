4cd6144fd81a7e4eab703e55245e70e9187dd6bd
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-12-07 16:21:39 +0530

    3.22

--- part3/models/person.js
+++ part3/models/person.js
@@ -1,35 +1,35 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+require('dotenv').config()
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url =process.env.URL;
+const url =process.env.URL
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-.then(res => console.log("Connected to mongo."))
-.catch(error => {
-  console.log(error.message);
-}
+  .then(res => console.log('Connected to mongo.'))
+  .catch(error => {
+    console.log(error.message)
+  }
   )
-  const personSchema = new mongoose.Schema({
-      name: {
+const personSchema = new mongoose.Schema({
+  name: {
     type: String,
     minlength: 3,
     required: true,
     unique: true
-    },
-number: {
+  },
+  number: {
     type: String,
     minlength: 8,
     required: true,
     unique: true
-}
+  }
 })
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 personSchema.set('toJSON', {
-transform: (document, returnedObject) => {
+  transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-}
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
