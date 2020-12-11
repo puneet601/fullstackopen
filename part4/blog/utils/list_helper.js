4894010d7982c2eb0d1b82fd447d4fd9cb735986
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-12-11 18:50:13 +0530

    4.6

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -28,37 +28,5 @@ let x=blogs.map(b => checklikes(b))
     
 return obj
 }
-const mostBlogs = (blogs) => {
-        let authors=[{name:blogs[0].author,blogs:0}] 
-        let c=0
-        blogs.forEach(blog => {
-             c+=1 
-             console.log(blog.author)
-                let index=authors.findIndex(author => author.name === blog.author);
-                console.log("index is ",index);
-                if(index === -1)
-                {
-                    const newObj={
-                        name:blog.author,blogs:blog.count
-                  
-                } 
-                authors.concat(newObj)}
-                else{
-                    authors[index].blogs =authors[index].blogs + 1
-                }
-        }); 
-      
-        
-        let max={
-            name:"chucknorris",blogs:0
-        }
-        authors.map(author => {
-           if(author.blogs > max.blogs)
-           {max.name=author.name 
-        max.blogs=author.blogs}
-        })
-        console.log(max,authors,c)
-        return max;
-}
 
-module.exports = {dummy,totalLikes,favouriteBlog,mostBlogs}
+module.exports = {dummy,totalLikes,favouriteBlog}
