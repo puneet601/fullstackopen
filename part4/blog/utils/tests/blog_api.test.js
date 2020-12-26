e6e28a7a42bbbdf8a558b610e177345b090a927d
Puneet K Jattana <puneetkaurjattana@gmail.com>
2020-12-26 17:46:17 +0530

    4.9

--- part4/blog/utils/tests/blog_api.test.js
+++ part4/blog/utils/tests/blog_api.test.js
@@ -4,12 +4,12 @@ const app = require('../../app')
 
 const api = supertest(app)
 const Blog = require('../../models/blog');
-const initialBlogs = [{ title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
-{
-  title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra",
+const initialBlogs = [
+  { title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
+{  title: "Go To Statement Considered Harmful", author: "Edsger W. Dijkstra",
   url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", likes: 5, __v: 0
   }];
-  beforeEach(async () => {
+  beforeEach(async () => { 
     await Blog.deleteMany({})
     let blogObject = new Blog(initialBlogs[0])
     await blogObject.save()
@@ -26,6 +26,10 @@ test('correct amount of blogs are returned', async () => {
   const response = await api.get('/api/blogs')
    expect(response.body).toHaveLength(initialBlogs.length)
     
+})
+test('id is stored as id not _id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
