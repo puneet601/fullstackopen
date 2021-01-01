b7b7a9e35e82704e9a9c0679d30dc4c2e4ad895e
Puneet K Jattana <puneetkaurjattana@gmail.com>
2021-01-01 16:17:59 +0530

    4.14

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogRouter= require('express').Router()
+const { request } = require('express')
+const { response } = require('../app')
 const app = require('../app')
 const blog = require('../models/blog')
 const Blog=require('../models/blog')
@@ -12,8 +14,8 @@ blogRouter.get('/:id', async (request, response) => {
   response.json(blog)
   })
   
-  blogRouter.post('/', async(request, response, next) => {
-    const body = new Blog(request.body) 
+  blogRouter.post('/', async(request, response) => {
+    const body = request.body
    const newBlog = new Blog({
       "title": body.title,
       "author": body.author,
@@ -25,10 +27,20 @@ blogRouter.get('/:id', async (request, response) => {
     
     
   })
-blogRouter.delete('/:id', async (request, response, next) => {
+blogRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
   console.log("deleted")
   response.status(204).end
   })
-
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const newblog = {
+    "title" :body.title,
+    "author": body.author,
+    "url": body.url,
+    "likes": body.likes + 1 
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newblog,{new:true})
+  response.json(updatedBlog)
+})
   module.exports=blogRouter
